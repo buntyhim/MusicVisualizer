de40a2a4bcb5b591265dd189e0f47351e65d45a6
Himanshu Yadav <bunty.him@live.com>
2017-11-05 00:45:12 +0530

    Update analyze.m

--- analyze.m
+++ analyze.m
@@ -1,13 +1,13 @@
 
 
-function analyze(a,i,nff,xff)
+function analyze(a,i,nff,xff)                               % nff is the segmented signal
      
-    x = fft(a(i:i+nff-2),nff);
+    x = fft(a(i:i+nff-2),nff);                              %finding fft of the segmented signal passed to this function 
     ar = area(xff(1:nff/2),abs(x(1:nff/2))/max(abs(x))); 
     ar.EdgeColor = 'none';
     ar.FaceColor = 'green';
     xlim([50 6e3]) ;
-    axis on ;
+    axis on ;                 % make it off if only waveform is required
     pause(0.01);
 
 end
