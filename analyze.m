e3936f0d95396355d33f0a952f66bf1c146fb6f6
Himanshu Yadav <bunty.him@live.com>
2017-11-05 00:48:54 +0530

    Update analyze.m

--- analyze.m
+++ analyze.m
@@ -1,6 +1,6 @@
 
 
-function analyze(a,i,nff,xff)                               % nff is the segmented signal
+function analyze(a,i,nff,xff)                               % nff is the complete signal
      
     x = fft(a(i:i+nff-2),nff);                              %finding fft of the segmented signal passed to this function 
     ar = area(xff(1:nff/2),abs(x(1:nff/2))/max(abs(x))); 
